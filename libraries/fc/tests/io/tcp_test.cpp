79651964f39f3a85b2a38956b32dd4235baac362
Li RuiGang <yueyemingming@163.com>
2018-06-22 15:24:19 +0800

    update libraries/fc/tests/io/tcp_test.cpp

--- libraries/fc/tests/io/tcp_test.cpp
+++ libraries/fc/tests/io/tcp_test.cpp
@@ -8,7 +8,7 @@ BOOST_AUTO_TEST_SUITE(tcp_tests)
 /***
  * Running this test through valgrind will show
  * a memory leak due to lack of logic in destructor.
- * See https://github.com/bitshares/bitshares-fc/blob/51688042b0b9f99f03224f54fb937fe024fe5ced/src/asio.cpp#L152
+ * See https://github.com/dbxchain/bitshares-fc/blob/51688042b0b9f99f03224f54fb937fe024fe5ced/src/asio.cpp#L152
  */
 BOOST_AUTO_TEST_CASE(tcpconstructor_test)
 {
