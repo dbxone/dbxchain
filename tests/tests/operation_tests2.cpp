71190d09a779998038e7868afa0a808f56cc5b16
Li RuiGang <yueyemingming@163.com>
2018-06-22 15:41:46 +0800

    update tests/tests/operation_tests2.cpp

--- tests/tests/operation_tests2.cpp
+++ tests/tests/operation_tests2.cpp
@@ -553,7 +553,7 @@ BOOST_AUTO_TEST_CASE( withdraw_permission_nominal_case )
 
 /**
  * Test asset whitelisting feature for withdrawals.
- * Reproduces https://github.com/bitshares/bitshares-core/issues/942 and tests the fix for it.
+ * Reproduces https://github.com/dbxchain/bitshares-core/issues/942 and tests the fix for it.
  */
 BOOST_AUTO_TEST_CASE( withdraw_permission_whitelist_asset_test )
 { try {
