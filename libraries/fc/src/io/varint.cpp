8d5a56b63bb2a2c57f0d32ffdf09eb150703cf15
Li RuiGang <yueyemingming@163.com>
2018-06-27 01:12:40 +0800

    update

---
+++ libraries/fc/src/io/varint.cpp
@@ -0,0 +1,10 @@
+#include <fc/io/varint.hpp>
+#include <fc/variant.hpp>
+
+namespace fc
+{
+void to_variant( const signed_int& var, variant& vo, uint32_t max_depth ) { vo = var.value; }
+void from_variant( const variant& var, signed_int& vo, uint32_t max_depth ) { vo.value = static_cast<int32_t>(var.as_int64()); }
+void to_variant( const unsigned_int& var, variant& vo, uint32_t max_depth )  { vo = var.value; }
+void from_variant( const variant& var, unsigned_int& vo, uint32_t max_depth )  { vo.value = static_cast<uint32_t>(var.as_uint64()); }
+}
