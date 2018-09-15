4772a250bbb3a3b20c2787b379585c837ee9b81d
Li RuiGang <yueyemingming@163.com>
2018-09-15 16:46:42 +0800

    update contract code

--- libraries/app/database_api.cpp
+++ libraries/app/database_api.cpp
@@ -338,6 +338,11 @@ fc::variants database_api_impl::get_objects(const vector<object_id_type>& ids)co
    return result;
 }
 
+static void copy_inline_row(const key_value_object& obj, vector<char>& data) {
+   data.resize( obj.value.size() );
+   memcpy( data.data(), obj.value.data(), obj.value.size() );
+}
+
 //////////////////////////////////////////////////////////////////////
 //                                                                  //
 // Subscriptions                                                    //
