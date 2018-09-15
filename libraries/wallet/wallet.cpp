5ae8eb81b1f1405567858fb590f54c133f0932fe
Li RuiGang <yueyemingming@163.com>
2018-09-15 16:17:06 +0800

    update libraries/wallet/wallet.cpp

--- libraries/wallet/wallet.cpp
+++ libraries/wallet/wallet.cpp
@@ -2907,7 +2907,7 @@ public:
    static_variant_map _operation_which_map = create_static_variant_map< operation >();
 
 #ifdef __unix__
-   mode_t                  _old_umask;
+   unsigned int  _old_umask;
 #endif
    const string _wallet_filename_extension = ".wallet";
 };
