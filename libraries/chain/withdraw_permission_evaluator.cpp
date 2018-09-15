6f6eba38b7c06552bd87c47a5b877cc86958ef40
Li RuiGang <yueyemingming@163.com>
2018-09-15 16:43:44 +0800

    update apply

--- libraries/chain/withdraw_permission_evaluator.cpp
+++ libraries/chain/withdraw_permission_evaluator.cpp
@@ -44,7 +44,9 @@ void_result withdraw_permission_create_evaluator::do_evaluate(const operation_ty
    return void_result();
 } FC_CAPTURE_AND_RETHROW( (op) ) }
 
-object_id_type withdraw_permission_create_evaluator::do_apply(const operation_type& op)
+//liruigang20180913 contract
+//object_id_type withdraw_permission_create_evaluator::do_apply(const operation_type& op)
+object_id_type withdraw_permission_create_evaluator::do_apply(const operation_type& op, uint32_t billed_cpu_time_us)
 { try {
    return db().create<withdraw_permission_object>([&op](withdraw_permission_object& p) {
       p.withdraw_from_account = op.withdraw_from_account;
