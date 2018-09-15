d4b8197a467ca928417e108f00627ff8f5566e5b
Li Ruigang <yueyemingming@163.com>
2018-09-15 09:11:14 +0000

    update to dbx

--- contracts/examples/linear_vesting_asset/linear_vesting_asset.cpp
+++ contracts/examples/linear_vesting_asset/linear_vesting_asset.cpp
@@ -8,7 +8,7 @@
 
 using namespace graphene;
 
-static const uint64_t contract_asset_id = 1;//GXS
+static const uint64_t contract_asset_id = 1;//DBX
 
 class linear_vesting_asset : public contract
 {
