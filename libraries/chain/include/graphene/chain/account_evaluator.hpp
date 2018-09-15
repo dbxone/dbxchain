6b55d3994bbe05e7dd25b0fd073b1d2b36d1e5bc
Li RuiGang <yueyemingming@163.com>
2018-09-15 16:24:35 +0800

    update contract code

--- libraries/chain/include/graphene/chain/account_evaluator.hpp
+++ libraries/chain/include/graphene/chain/account_evaluator.hpp
@@ -44,7 +44,9 @@ public:
    typedef account_update_operation operation_type;
 
    void_result do_evaluate( const account_update_operation& o );
-   void_result do_apply( const account_update_operation& o );
+   //liruigang20180913 contract
+   //void_result do_apply( const account_update_operation& o );
+   void_result do_apply( const account_update_operation& o, uint32_t billed_cpu_time_us = 0);
 
    const account_object* acnt;
 };
