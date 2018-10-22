c136381fc83143b7d18df5f5365ea83a3fd11f98
Li RuiGang <yueyemingming@163.com>
2018-10-22 11:48:01 +0800

    update

--- contracts/examples/test/contract_storage_demo/contract_storage_demo.cpp
+++ contracts/examples/test/contract_storage_demo/contract_storage_demo.cpp
@@ -1,7 +1,7 @@
-#include <dbxlib/contract.hpp>
-#include <dbxlib/dispatcher.hpp>
-#include <dbxlib/multi_index.hpp>
-#include <dbxlib/types.h>
+#include <graphenelib/contract.hpp>
+#include <graphenelib/dispatcher.hpp>
+#include <graphenelib/multi_index.hpp>
+#include <graphenelib/types.h>
 #include <string>
 
 using namespace graphene;
