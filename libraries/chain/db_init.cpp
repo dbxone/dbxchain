4afdd11227e48e3ad138fed061ee92e640305a46
Li RuiGang <yueyemingming@163.com>
2018-10-29 21:30:27 +0800

    update

--- libraries/chain/db_init.cpp
+++ libraries/chain/db_init.cpp
@@ -189,7 +189,6 @@ void database::initialize_evaluators()
 
 void database::initialize_indexes()
 {
-	ilog( "initialize_indexes 1111111111111111111 " );
    reset_indexes();
    _undo_db.set_max_size( GRAPHENE_MIN_UNDO_HISTORY );
 
@@ -233,12 +232,13 @@ void database::initialize_indexes()
 
    add_index< primary_index< simple_index< fba_accumulator_object       > > >();
 
-   ilog( "initialize_indexes 222222222222222 " );
+   ilog( "initialize_indexes 111111111111 " );
    add_index< primary_index< table_id_multi_index> >();  //liruigang20181029 contract
+   ilog( "initialize_indexes 222222222222 " );
    add_index< primary_index< key_value_index> >();  //liruigang20181029 contract
+   ilog( "initialize_indexes 33333333333 " );
    add_index< primary_index< index64_index> >();  //liruigang20181029 contract
-
-   ilog( "initialize_indexes 333333333333 " );
+   ilog( "initialize_indexes 44444444444 " );
 }
 
 void database::init_genesis(const genesis_state_type& genesis_state)
